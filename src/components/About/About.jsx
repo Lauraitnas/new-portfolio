2c3d452efdf05268a1f66d6d4533c915a184f504
Laura Santi <89958677+Lauraitnas@users.noreply.github.com>
2022-06-13 15:39:02 +0800

    mobile responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,17 +12,16 @@ function About(){
 
     return(
         <div className="main_div" id="about">
-            <div className="main_div1">
-                <div class="div_1">
-                    <h1>Full Stack Developer</h1>
-                    <p>My highest aspiration is to become a full-stack developer and potentially a software engineer working for companies that can actually make a difference in people every day’s life. I first discovered my passion for coding a year ago, while trying to create an online presence for my family’s business back in Italy. When Covid first hit, they didn’t have any kind of “digital real-estate”. So I taught myself how to create a website using WordPress to help them stay afloat during the health crisis.</p>
-                    <a class="btn" href="https://www.linkedin.com/in/laura-santi3/" target="_blank"><span> MORE ABOUT ME</span></a>
-                </div>
-                <div class="div_1">
-                    <img id="profile" src={mypic} alt="Laura Santi"></img>
-                </div>
+            <div class="div_1">
+                <h1>Full Stack Developer</h1>
+                <p>My highest aspiration is to become a full-stack developer and potentially a software engineer working for companies that can actually make a difference in people every day’s life. I first discovered my passion for coding a year ago, while trying to create an online presence for my family’s business back in Italy. When Covid first hit, they didn’t have any kind of “digital real-estate”. So I taught myself how to create a website using WordPress to help them stay afloat during the health crisis.</p>
+                <div className="a"><a class="btn" href="https://www.linkedin.com/in/laura-santi3/" target="_blank"><span> MORE ABOUT ME</span></a></div>
+            </div>
+            <div class="div_1">
+                <img id="profile" src={mypic} alt="Laura Santi"></img>
             </div>
         </div>
+
     )
 }
 
